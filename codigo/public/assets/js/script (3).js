dd430be005ab833fa955eea71d0b45290a268362
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2024-10-26 01:56:03 -0300

    1.0

---
+++ codigo/public/assets/js/script (3).js
@@ -0,0 +1,9 @@
+const signupButton = document.querySelector('.signup');
+const loginButton = document.querySelector('.login-btn');
+
+signupButton.addEventListener('click', () => {
+  
+  console.log("Signup button clicked");
+});
+
+loginButton.addEventListener('click', () => {
