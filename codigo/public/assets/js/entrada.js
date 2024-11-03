c913a99c9f4cf1ef29560aebd8145e4feb598f5c
João Ricardo Fiuza <156984266+JaoRicFiuza@users.noreply.github.com>
2024-11-03 11:21:41 -0300

    Update entrada.js

--- codigo/public/assets/js/entrada.js
+++ codigo/public/assets/js/entrada.js
@@ -2,6 +2,11 @@ function saveUserInfo() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
+  if (!username || !password) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
   const userInfo = {
     username: username,
     password: password
@@ -9,23 +14,30 @@ function saveUserInfo() {
 
   const userInfoJSON = JSON.stringify(userInfo);
 
-  console.log(userInfoJSON);
+  console.log('Saving user info:', userInfoJSON);
 
   localStorage.setItem('userInfo', userInfoJSON);
   alert('Informações salvas com sucesso!');
+  
+  window.location.href = 'home.html'; 
 }
 
 function loginUser() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
+  if (!username || !password) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
   const storedUserInfo = localStorage.getItem('userInfo');
   if (storedUserInfo) {
     const userInfo = JSON.parse(storedUserInfo);
     if (userInfo.username === username && userInfo.password === password) {
-      alert('Login bem-sucedido!');
+      alert('Bem-vindo vamos começar os treinos?');
       
-      window.location.href = 'untitled-1.html'; 
+      window.location.href = 'home.html'; 
     } else {
       alert('Nome de usuário ou senha incorretos.');
     }
