eb863d09272e189a16030d83b9572e49992a187d
Joao Ricardo Fiuza <joaoricfiuza@gmail.com>
2024-11-04 18:12:34 -0300

    commit joao

---
+++ codigo/Individual_commits/João_Ricardo/scriptperfilusuario.js
@@ -0,0 +1,87 @@
+const editProfileButton = document.querySelector('.edit-profile');
+const bioText = document.getElementById('bio-text');
+const editBioText = document.getElementById('edit-bio-text');
+const goalItems = document.querySelectorAll('.goal-item');
+const addStarButton = document.getElementById('add-star-button');
+const starsContainer = document.getElementById('stars-container');
+const profilePicture = document.getElementById('profile-picture');
+const navProfilePicture = document.getElementById('nav-profile-picture');
+const uploadImage = document.getElementById('upload-image');
+const changeProfileButton = document.getElementById('change-profile-button');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const userProfile = JSON.parse(localStorage.getItem('userProfile')) || {};
+  
+  if (userProfile.profilePicture) {
+    profilePicture.src = userProfile.profilePicture;
+    navProfilePicture.src = userProfile.profilePicture;
+  }
+  
+  if (userProfile.bio) {
+    bioText.textContent = userProfile.bio;
+  }
+});
+
+editProfileButton.addEventListener('click', () => {
+  bioText.style.display = bioText.style.display === 'none' ? 'block' : 'none';
+  editBioText.style.display = editBioText.style.display === 'none' ? 'block' : 'none';
+
+  goalItems.forEach(goalItem => {
+    const goalTextArea = goalItem.querySelector('.edit-area');
+    const goalText = goalItem.querySelector('span');
+
+    goalTextArea.style.display = goalTextArea.style.display === 'none' ? 'block' : 'none';
+    goalText.style.display = goalText.style.display === 'none' ? 'block' : 'none';
+  });
+});
+
+goalItems.forEach(goalItem => {
+  const goalTextArea = goalItem.querySelector('.edit-area');
+  goalTextArea.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const goalText = goalItem.querySelector('span');
+      goalText.textContent = goalTextArea.value;
+      goalTextArea.style.display = 'none';
+      goalText.style.display = 'block';
+    }
+  });
+});
+
+editBioText.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    bioText.textContent = editBioText.value;
+    editBioText.style.display = 'none';
+    bioText.style.display = 'block';
+
+    const userProfile = JSON.parse(localStorage.getItem('userProfile')) || {};
+    userProfile.bio = editBioText.value;
+    localStorage.setItem('userProfile', JSON.stringify(userProfile));
+  }
+});
+
+addStarButton.addEventListener('click', () => {
+  const newStar = document.createElement('i');
+  newStar.classList.add('fas', 'fa-star');
+  starsContainer.appendChild(newStar);
+});
+
+changeProfileButton.addEventListener('click', () => {
+  uploadImage.click();
+});
+
+uploadImage.addEventListener('change', () => {
+  if (uploadImage.files && uploadImage.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      profilePicture.src = e.target.result;
+      navProfilePicture.src = e.target.result;
+      
+      const userProfile = JSON.parse(localStorage.getItem('userProfile')) || {};
+      userProfile.profilePicture = e.target.result;
+      localStorage.setItem('userProfile', JSON.stringify(userProfile));
+    };
+
+    reader.readAsDataURL(uploadImage.files[0]);
+  }
+});
---
+++ codigo/Individual_commits/joao_oliveira/scriptTreinos.js
@@ -0,0 +1,135 @@
+
+
+
+class Treino {
+    constructor() {
+        this.id = 1;
+        this.arrayTreinos = [];
+    }
+
+    salvar() {
+        let treino = this.lerDados();
+
+        if (this.validaCampos(treino)) {
+            this.adicionar(treino);
+            this.atualizaTabela();
+            this.cancelar(); 
+            fecharPopup(); 
+        }
+    }
+
+    atualizaTabela() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerHTML = '';
+
+        for (let i = 0; i < this.arrayTreinos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_treino = tr.insertCell();
+            let td_repeticao = tr.insertCell();
+
+            td_treino.innerText = this.arrayTreinos[i].nomeTreino;
+            td_repeticao.innerText = this.arrayTreinos[i].quantidadeRepeticoes;
+        }
+    }
+
+    adicionar(treino) {
+        this.arrayTreinos.push(treino);
+        this.id++;
+    }
+
+    lerDados() {
+        let treino = {};
+        treino.id = this.id;
+        treino.nomeTreino = document.getElementById('treino').value;
+        treino.quantidadeRepeticoes = document.getElementById('repeticao').value;
+        return treino;
+    }
+
+    validaCampos(treino) {
+        let msg = '';
+        if (treino.nomeTreino === '') {
+            msg += '- Informe o treino\n';
+        }
+
+        if (treino.quantidadeRepeticoes === '') {
+            msg += '- Informe a repetição';
+        }
+
+        if (msg !== '') {
+            alert(msg);
+            return false;
+        }
+
+        return true;
+    }
+
+    cancelar() {
+        document.getElementById('treino').value = '';
+        document.getElementById('repeticao').value = '';
+    }
+}
+
+var treino = new Treino();
+
+var treinosCard1 = [];
+var treinosCard2 = [];
+var treinosCard3 = [];
+var treinoIdAtual;
+
+function abrirPopup(treinoId) {
+    const popup = document.getElementById('popup');
+    const treinoDetalhes = document.getElementById('treino-detalhes');
+
+    treinoIdAtual = treinoId;
+    popup.style.display = "flex";
+
+    atualizarListaTreinosPopup();
+}
+
+function fecharPopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = "none";
+}
+
+function adicionarTreinoPopup(treinoId) {
+    const nomeTreino = document.getElementById('nome-treino-popup').value;
+    const repeticao = document.getElementById('repeticao-popup').value;
+
+
+    const listaTreinosPopup = document.getElementById('lista-treinos-popup');
+    const novoTreinoPopup = document.createElement('li');
+    novoTreinoPopup.textContent = `Nome: ${nomeTreino} - Repetição: ${repeticao}`;
+    listaTreinosPopup.appendChild(novoTreinoPopup);
+
+
+    const listaTreinosCard = document.getElementById(`lista-treinos-${treinoId}`);
+    const novoTreinoCard = document.createElement('li');
+    novoTreinoCard.textContent = `Nome: ${nomeTreino} - Repetição: ${repeticao}`;
+    listaTreinosCard.appendChild(novoTreinoCard);
+
+
+    document.getElementById('nome-treino-popup').value = '';
+    document.getElementById('repeticao-popup').value = '';
+}
+
+
+function atualizarListaTreinosPopup() {
+    const lista = document.getElementById('lista-treinos-popup');
+    lista.innerHTML = '';
+
+    let treinosAtuais = [];
+    if (treinoIdAtual === 1) {
+        treinosAtuais = treinosCard1;
+    } else if (treinoIdAtual === 2) {
+        treinosAtuais = treinosCard2;
+    } else if (treinoIdAtual === 3) {
+        treinosAtuais = treinosCard3;
+    }
+
+    treinosAtuais.forEach(t => {
+        const li = document.createElement('li');
+        li.innerText = `Treino: ${t.nome} - Repetições: ${t.repeticao}`;
+        lista.appendChild(li);
+    });
+}
